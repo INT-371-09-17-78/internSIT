4cfe11854672556dd789d7a9d7bbecad9f74b220
J-Tiny <60166962+J-Tiny@users.noreply.github.com>
2022-04-19 00:25:14 +0700

    Revert "Feature/basic login"

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -2,16 +2,16 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 
 export default class UsersController {
-  public async index() {
-    return await User.all()
-  }
-  public async store() {
-    return await User.create({
-      username: 'virk',
-      password: 'virk@adonisjs.com',
-    })
-  }
-  public async show({ params }: HttpContextContract) {
-    return await User.find(params.id)
-  }
+    public async index() {
+        return await User.all();
+    }
+    public async store() {
+        return await User.create({
+            username: 'virk',
+            password: 'virk@adonisjs.com',
+          })
+    }
+    public async show({params}:HttpContextContract) {
+        return await User.find(params.id)
+    }
 }
