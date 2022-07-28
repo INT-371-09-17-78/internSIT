8aed0fe0aca6901d712919d7611947ce3f1511cc
J-Tiny <jirawath.anan@gmail.com>
2022-07-29 01:11:36 +0700

    delete file and preview file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import '../css/app.css'
+
+window.onload = () => {
+  if (document.getElementById('topic').value && document.getElementById('content').value)
+    document.getElementById('sendButton').disabled = false
+}
