43d4f75f2889ff700f3cf2a6a93b9968407d4597
Kampol-Nui <kampol.43@mail.kmutt.ac.th>
2022-09-07 23:16:55 +0700

    script

---
+++ init.sh
@@ -0,0 +1,17 @@
+INSERT INTO documents (doc_name)
+VALUES ('TR-01');
+INSERT INTO documents (doc_name)
+VALUES ('TR-02');
+INSERT INTO documents (doc_name)
+VALUES ('TR-03 and TR-05 (1/6)');
+INSERT INTO documents (doc_name)
+VALUES ('Informed supervision (1/6)');
+INSERT INTO documents (doc_name)
+VALUES ('TR-03 and TR-05 (2/6)');
+
+INSERT INTO statuses (status_name)
+VALUES ('Approve');
+INSERT INTO statuses (status_name)
+VALUES ('Pending');
+INSERT INTO statuses (status_name)
+VALUES ('Not Approve');
