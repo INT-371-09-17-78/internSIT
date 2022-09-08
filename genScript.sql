a2c2397d95d67c0adabafddd2035c8a2dcdc9ed8
Kampol-Nui <kampol.43@mail.kmutt.ac.th>
2022-09-08 22:01:07 +0700

    test script

---
+++ genScript.sql
@@ -0,0 +1,5 @@
+INSERT INTO documents (doc_name) VALUES ('TR-01');
+INSERT INTO documents (doc_name) VALUES ('TR-02');
+INSERT INTO documents (doc_name) VALUES ('TR-03 and TR-05 (1/6)');
+INSERT INTO documents (doc_name) VALUES ('Informed supervision (1/6)');
+INSERT INTO documents (doc_name) VALUES ('TR-03 and TR-05 (2/6)');
