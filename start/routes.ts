915518a414910383404365c7f94d5235e9267edc
J-Tiny <jirawath.anan@gmail.com>
2022-11-15 07:29:41 +0700

    fix everyting

--- start/routes.ts
+++ start/routes.ts
@@ -30,6 +30,18 @@ View.global('middleEllipsis', (str: string) => {
   return str
 })
 
+View.global('formatBytes', (bytes: number, decimals = 2) => {
+  if (!+bytes) return '0 Bytes'
+
+  const k = 1024
+  const dm = decimals < 0 ? 0 : decimals
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
+})
+
 View.global('getCurrentYear', () => {
   return new Date().getFullYear()
 })
